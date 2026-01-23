8d1b430b84c3d72e87032855ad71b5df9ea8ab24
FarahNagy <farahalaa0529@outlook.com>
2026-01-24 00:22:58 +0200

    lab 5

--- groceryTracker/groceryTracker.js
+++ groceryTracker/groceryTracker.js
@@ -3,6 +3,10 @@ function groceryTracker() {
     const amount2 = parseFloat(document.getElementById('2').value);
     const amount3 = parseFloat(document.getElementById('3').value);
 
-    const total = amount1 + amount2 + amount3;
+    let total = amount1 + amount2 + amount3;
+    console.log("the type is " + (typeof total))
+    if (isNaN(total)) 
+        total = 0;
+
     document.getElementById('result').innerText = `The total amount of all groceries is : ${total}`;
 }
