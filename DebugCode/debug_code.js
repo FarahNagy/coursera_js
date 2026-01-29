b7a3576286acd550d5d4c19dd6a80ca40404a992
FarahNagy <farahalaa0529@outlook.com>
2026-01-30 00:41:07 +0200

    lab 11 practice task

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,10 +3,12 @@ function performOperation() {
     // Get user input from input fields
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
+    let operation = document.getElementById('operation').value;
+
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
         // Perform the operation
-        let result = multiply(num1, num2);
+        let result = calculate('a', num2, operation);
 
         // Display the result
         displayResult(result);
@@ -21,6 +23,16 @@ function multiply(a, b) {
 
     // Multiply the numbers
     return a * b;
+}
+
+function calculate(a, b, operation) {
+    debugger;
+    if (operation === 'add')
+        return a + b;
+    if (operation === 'multiply')
+        return a * b;
+    if (operation === 'divide')
+        return a / b;
 }
 
 function displayResult(result) {
